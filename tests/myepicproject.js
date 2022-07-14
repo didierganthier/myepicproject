e665e33d0f80730a6c208cfa2c74c12a103da198
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-07-14 08:47:01 -0400

    post and get data

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -26,19 +26,23 @@ const main = async () => {
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString())
 
-  await program.rpc.addGif({
+  await program.rpc.addGif("https://media.giphy.com/media/IL4iTvQH0MjS/giphy.gif", {
     accounts: {
       baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
     },
   });
 
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
   try {
-    await main();process.exit(0);
+    await main(); process.exit(0);
   } catch (error) {
     console.error(error);
     process.exit(1);
